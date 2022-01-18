35a9e77dc021d57369232f6dc9bb9a85c79db696
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-18 18:27:38 -0500

    Create SafeCookie.php

---
+++ Potter/Cookie/Safe/SafeCookie.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Potter\Cookie\Safe;
+
+final class SafeCookie extends AbstractSafeCookie
+{
+    use CookieDomainTrait, CookieScriptingTrait, SecureCookieTrait;
+
+    public function __construct(private string $name, string $value, int $expiry = 0)
+    {
+        $this->setValue($value);
+        $this->setExpiry($expiry);
+        $this->matchProtocol();
+        $this->blockCrossDomain();
+        $this->blockScripting();
+        $this->set();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    private function matchProtocol(): void
+    {
+        if (isset($_SERVER['HTTPS']) || empty($_SERVER['HTTPS']))
+            $this->setPlain();
+            return;
+        $this->secure();
+    }
+}
