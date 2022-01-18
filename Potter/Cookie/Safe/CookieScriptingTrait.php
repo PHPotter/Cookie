aa498bbe813643771b4abacfe4e719e044de65f3
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-18 18:26:59 -0500

    Create CookieScriptingTrait.php

---
+++ Potter/Cookie/Safe/CookieScriptingTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Potter\Cookie\Safe;
+
+trait CookieScriptingTrait
+{
+    final public function allowScripting(): void
+    {
+        $this->setOption(SafeCookieInterface::HTTPONLY, false);
+    }
+
+    final public function blockScripting(): void
+    {
+        $this->setOption(SafeCookieInterface::HTTPONLY, true);
+    }
+
+    abstract public function setOption(string $option, string $value): void;
+}
